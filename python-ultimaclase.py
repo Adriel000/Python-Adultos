56073e83c090614da47a7537937782f5aed8e18c
adriel <adrielherrera757@gmail.com>
2023-06-12 15:23:26 -0300

    mi primer commit

---
+++ python-ultimaclase.py
@@ -0,0 +1,41 @@
+'''#entradas
+turno=int(input('Introduzca su turno de trabajo:'))
+horas=float(input('Introduzca sus horas trabajas según el turno:'))
+diurno=1
+nocturno=2
+
+#procesos
+if turno==1:
+    Salario: print(5234.5*horas)
+    
+elif turno==2:
+   Salario: print(8049.75*horas)
+    
+else:
+    print('Introduzca un valor correcto:')
+
+
+print('El salario es:',salario)'''
+
+
+
+'''#entradas
+fondo=int(input('Ingrese la medida del fondo de su terreno:'))
+frente=int(input('Ingrese la medida del frente de su terreno:'))
+
+
+#procesos
+if frente==fondo:
+    perimetro=(frente*4)
+    superficie=(frente**2)
+    print('La superficie de un cuadrado es:',superficie,
+      'El perimetro de un cuadrado es:',perimetro)
+
+else:
+    perimetro=(fondo*2+frente*2)
+    superficie=(frente*fondo)
+    print('La superficie de un rectangulo es:', superficie,
+      'El perimetro de un rectangulo es:',perimetro)'''
+
+        
+        
